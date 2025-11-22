9774bfea411f3544b131fcc0f33c9ec2a08c0de9
Jake Peters <jake@iamjakepeters.com>
2025-11-22 11:43:43 -0600

    Update Nitro static site config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,14 @@
 import { defineNitroConfig } from 'nitro/config'
 
 export default defineNitroConfig({
+  preset: 'static',
   static: true,
   prerender: {
     crawlLinks: true,
     routes: ['/'],
+  },
+  output: {
+    dir: '.output',
+    publicDir: '.output/public',
   },
 })
